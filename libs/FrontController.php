b1c45f01deb9b77e2ec492af7dac469d2abe278f
Alejandro Gamez <manuel.gamez@cfe.mx>
2020-04-24 14:03:28 -0500

    Recibir nombre del controlador y accion en el frontController

--- libs/FrontController.php
+++ libs/FrontController.php
@@ -1,6 +1,16 @@
 <?php
 class FrontController{
 	public static function main(){
+		$controller = $_GET['controller'];
+		$action = $_GET['action'];
+		
+		if(!$controller){/** En caso de que no venga el controlador*/
+			$controller='index';
+		}
+		if(!$action){/** En caso de que no venga la accion*/
+			$action='index';
+		}
+		echo "Invocando la accion $action del controlador $controller";
 	}
 }
 ?>
